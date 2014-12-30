ead83de2c7470a85a6f9dc791b4ed1525f141ec3
Stefan Mehnert <stefanmehnert@gmail.com>
2014-12-30 10:15:23 +0100

    Error Handling extracted to package zs40x:errors

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -13,12 +13,12 @@ Template.postSubmit.events({
 
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
-      if (error)
-        return throwError(error.reason);
+      if (error) 
+        Errors.throw(error.reason);
 			
 			// show this result but route anyway
       if (result.postExists)
-        throwError('This link has already been posted');
+        Errors.throw('This link has already been posted');
 			
       Router.go('postPage', {_id: result._id});  
     });
---
+++ packages/zs40x:errors/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'zs40x:errors',
+  summary: 'A pattern to display application errors to the user',
+  version: '1.0.0'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+
+  if (api.export) 
+    api.export('Errors');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('zs40x:errors');
+  api.addFiles('zs40x:errors-tests.js');
+});

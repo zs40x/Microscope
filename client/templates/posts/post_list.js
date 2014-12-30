12b4bcf2c90f41e059ba795572d62b28697aa42d
Stefan Mehnert <stefanmehnert@gmail.com>
2014-12-30 20:29:35 +0100

    Added animations for posts

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,5 +1,8 @@
 Template.postsList.helpers({
-  posts: function() {
-    return Posts.find({}, {sort: {submitted: -1}});
+  postsWithRank: function() {
+    return this.posts.map(function(post, index, cursor) {
+      post._rank = index;
+      return post;
+    });
   }
 });

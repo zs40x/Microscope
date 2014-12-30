0cb5746a726d58cbc729aeb685beb94dd9c32a82
Stefan Mehnert <stefanmehnert@gmail.com>
2014-12-30 10:19:41 +0100

    Added Tests to zs40x:errors Package'

--- packages/zs40x:errors/package.js
+++ packages/zs40x:errors/package.js
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('zs40x:errors');
-  api.addFiles('zs40x:errors-tests.js');
+  api.use(['tinytest', 'test-helpers'], 'client');  
+  api.use('zs40x:errors','client');
+  api.addFiles('zs40x:errors-tests.js','client');
 });
